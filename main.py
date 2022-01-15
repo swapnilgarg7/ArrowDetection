74b6a9c5ad06d20a7fd54797f162ef0d329515ea
Swapnil Garg <32569674+swapnilgarg7@users.noreply.github.com>
2022-01-16 02:37:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,14 +9,9 @@ def initialize():
     cap.set(4, frameHeight) #sets height of frame as frameHeight(480)
     cap.set(10,150) #brightness level
 
-def detectArrow(img):
-    imgHSV = cv.cvtColor(img, cv.COLOR_BGR2HSV) #converting image to HSV
-    
-    lower = np.array([0,50,20]) #lower HSV value for red colour
-    upper = np.array([10,255,255]) #upper HSV value for red colour
-    mask = cv.inRange(imgHSV, lower, upper) #create mask of red-coloured objects
+def detectArrow(img,imgCanny):
 
-    contours, heirarchy = cv.findContours(mask, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
+    contours, heirarchy = cv.findContours(imgCanny, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE) 
     #finds outlines of that mask
 
     for cnt in contours:
@@ -28,12 +23,24 @@ def detectArrow(img):
             approx = cv.approxPolyDP(cnt, 0.02*peri, True) #gives corners of the mask
             objCor = len(approx) #to store number of corners
 
-            x, y, w, h = cv.boundingRect(approx) #to find dimensions of shape
-            pos = (x+(w//2), y+(h//2)) #get positon of centre of shape from dimensions
-
             if objCor==7: #arrow has 7 corners
-                cv.putText(imgContour,"arrow",pos, cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,255),2) 
-                #temporary, for displaying if arrow got detected or not
+
+                x, y, w, h = cv.boundingRect(approx) #to find dimensions of shape
+                xpos,ypos = (x+(w//2), y+(h//2)) #get positon of centre of shape from dimensions
+                
+                hsvFrame = cv.cvtColor(img, cv.COLOR_BGR2HSV) #convert image to HSV coloured
+                centrePixel = hsvFrame[xpos,ypos] #get centre pixel of the shape as HSV
+                hueVal = centrePixel[0] #hue value is H of HSV
+
+                if hueVal<5:
+
+                    cv.putText(imgContour,"arrow",(xpos,ypos), cv.FONT_HERSHEY_COMPLEX, 0.5, (0,255,255),2) 
+                    #temporary, for displaying if arrow got detected or not
+
+            else:
+                break
+               
+
 
 
 #MAIN CODE STARTS HERE  
@@ -54,7 +61,7 @@ while True:
     imgBlur = cv.GaussianBlur(imgGray, (7,7), 1) #then blurs the greyscaled image
     imgCanny = cv.Canny(imgBlur,50,50) #finally converts the grey+blurred to Canny image(edges visible)
 
-    detectArrow(img) #current frame/image passed to the detectArrow function
+    detectArrow(img,imgCanny) #current frame/image passed to the detectArrow function
 
     cv.imshow("Original Camera Feed", img) #shows original camera feed
     cv.imshow('Arrow Detect',imgContour) #shows the image which shows if arrow detected or not
